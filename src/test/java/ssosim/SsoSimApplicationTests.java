31eac95454c76c3ec269197e776333e05df038ae
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-16 23:45:40 -0300

    build: install spring boot web service

---
+++ src/test/java/ssosim/SsoSimApplicationTests.java
@@ -0,0 +1,13 @@
+package ssosim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
