d55e9cb1398b08bd3a94b2e960a4820a2c2fc8c7
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-17 19:12:06 -0300

    refactor: extract the rest controller

---
+++ src/main/java/ssosim/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package ssosim.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello-world")
+public class HelloWorld {
+	
+	@GetMapping
+	public String hello() {
+		return String.format("Hello, World!");
+	}
+}
