adbd18dbd208871aad5b8cd12d6880c61a12944c
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-18 01:11:43 -0300

    feat: describe the api and the route /api/hello-world

--- src/main/java/ssosim/config/SwaggerConfiguration.java
+++ src/main/java/ssosim/config/SwaggerConfiguration.java
@@ -3,8 +3,10 @@ package ssosim.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,6 +19,16 @@ public class SwaggerConfiguration {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.ant("/api/*"))
+				.build()
+				.apiInfo(apiInfo())
+				.useDefaultResponseMessages(false);
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("SSOSim")
+				.description("Simple Operating System Simulator")
+				.version("0.0.2")
 				.build();
 	}
 	
