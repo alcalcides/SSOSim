82b0525f730e95236414fd583b5cdbb8a9efc3b1
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-18 00:39:41 -0300

    feat: set uri title

--- src/main/java/ssosim/controller/HelloWorld.java
+++ src/main/java/ssosim/controller/HelloWorld.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/hello-world")
 public class HelloWorld {
 	
 	@GetMapping
+	@ApiOperation("Return \"Hello, World!\" for troubleshooting.")
 	public String hello() {
 		return String.format("Hello, World!");
 	}
