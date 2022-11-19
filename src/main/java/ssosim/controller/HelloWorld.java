d1f5fc26b36c4edb3cdfd76d4ec6ce40d11ec5ed
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-19 17:04:11 -0300

    feat: install logging interface

--- src/main/java/ssosim/controller/HelloWorld.java
+++ src/main/java/ssosim/controller/HelloWorld.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/hello-world")
 @Api(description = "Just to debug")
+@Slf4j
 public class HelloWorld {
 	
 	@GetMapping
 	@ApiOperation("Return \"Hello, World!\" to troubleshooting.")
 	public String hello() {
+		log.info("get /api/hello-world");
 		return String.format("Hello, World!");
 	}
 }
