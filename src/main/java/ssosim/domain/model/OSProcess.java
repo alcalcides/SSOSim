e27b6352c205504476c3febae9c702ce8d5c3b70
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-24 00:14:11 -0300

    feat: allow a process get cpu

--- src/main/java/ssosim/domain/model/OSProcess.java
+++ src/main/java/ssosim/domain/model/OSProcess.java
@@ -4,14 +4,31 @@ public class OSProcess {
 	private String id;
 	private int executionTime;
 	private int arriveTime;
+	private int remainingTime;
+	private boolean fineshed;
 
 	public OSProcess(String id, int executionTime, int arriveTime) {
 		this.id = id;
 		this.executionTime = executionTime;
 		this.arriveTime = arriveTime;
+		setRemainingTime(executionTime);
 	}
 
-	// Getters and Setters
+	public void run() {
+		setRemainingTime(remainingTime - 1);
+	}
+	
+	public void setRemainingTime(int remaingTime) {
+		if (remaingTime <= 0) {
+			this.remainingTime = 0;
+			fineshed = true;
+		} else {
+			this.remainingTime = remaingTime;
+			fineshed = false;
+		}
+	}
+
+	// Normal Getters
 	public String getId() {
 		return id;
 	}
@@ -24,16 +41,12 @@ public class OSProcess {
 		return arriveTime;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public int getRemainingTime() {
+		return remainingTime;
 	}
 
-	public void setExecutionTime(int executionTime) {
-		this.executionTime = executionTime;
-	}
-
-	public void setArriveTime(int arriveTime) {
-		this.arriveTime = arriveTime;
+	public boolean isFineshed() {
+		return fineshed;
 	}
 
 }
