f370be32ae8baac5b59226888679e7bd2fb03e75
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-11-25 21:25:59 -0300

    refactor: operating system creates the scheduler

--- src/main/java/ssosim/domain/model/OperatingSystem.java
+++ src/main/java/ssosim/domain/model/OperatingSystem.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import ssosim.domain.model.metaData.Journal;
 import ssosim.domain.model.processManagement.OSProcess;
 import ssosim.domain.model.processManagement.ProcessManager;
+import ssosim.domain.model.processManagement.scheduler.SchedulerFIFO;
 
 @Slf4j
 public class OperatingSystem {
@@ -14,7 +15,7 @@ public class OperatingSystem {
 
 	public OperatingSystem(ArrayList<OSProcess> processes) {
 		log.info(">> creating operating system");
-		processManager = new ProcessManager(processes);
+		processManager = new ProcessManager(processes, new SchedulerFIFO());
 		time = 0;
 	}
 
--- src/main/java/ssosim/domain/model/processManagement/ProcessManager.java
+++ src/main/java/ssosim/domain/model/processManagement/ProcessManager.java
@@ -4,20 +4,18 @@ import java.util.ArrayList;
 
 import ssosim.domain.model.metaData.Journal;
 import ssosim.domain.model.processManagement.scheduler.Scheduler;
-import ssosim.domain.model.processManagement.scheduler.SchedulerFIFO;
 
 public class ProcessManager {
-	private int quantum = 1;
 	private ArrayList<OSProcess> processes;
 	private Scheduler scheduler;
 
-	public ProcessManager(ArrayList<OSProcess> processes) {
-		setProcesses(processes);
-		this.scheduler = new SchedulerFIFO();
+	public ProcessManager(ArrayList<OSProcess> processes, Scheduler scheduler) {
+		this.processes = processes;
+		this.scheduler = scheduler;
 	}
 
 	public Journal run() {
-		Journal journalCPU = scheduler.run(processes);
+		Journal journal = scheduler.run(processes);
 
 		float turnAround = 0.0f;
 		for (OSProcess process : processes) {
@@ -25,21 +23,13 @@ public class ProcessManager {
 		}
 		turnAround /= processes.size();
 
-		journalCPU.setTurnAround(turnAround);
+		journal.setTurnAround(turnAround);
 
-		return journalCPU;
-	}
-
-	public int getQuantum() {
-		return quantum;
+		return journal;
 	}
 
 	public ArrayList<OSProcess> getProcesses() {
 		return processes;
-	}
-
-	public void setProcesses(ArrayList<OSProcess> processes) {
-		this.processes = processes;
 	}
 
 	public Scheduler getScheduler() {
