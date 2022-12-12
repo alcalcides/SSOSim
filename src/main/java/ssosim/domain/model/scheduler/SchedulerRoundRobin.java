a371604a84307b5b1b7ceaae0056517399114821
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-12-12 01:26:34 -0300

    feat: launch Round Robin algorithm

--- src/main/java/ssosim/domain/model/scheduler/SchedulerRoundRobin.java
+++ src/main/java/ssosim/domain/model/scheduler/SchedulerRoundRobin.java
@@ -1,16 +1,17 @@
 package ssosim.domain.model.scheduler;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
 import ssosim.domain.model.metaData.Journal;
 import ssosim.domain.model.processManagement.OSProcess;
 import ssosim.domain.model.processManagement.ProcessManager;
-import ssosim.domain.model.processManagement.ReadyProcessQueue;
 
 @Slf4j
 public class SchedulerRoundRobin extends PreemptiveScheduler {
-	private ReadyProcessQueue readyProcessesQueue;
 	private int time;
 
 	public SchedulerRoundRobin(int quantum) {
@@ -23,7 +24,50 @@ public class SchedulerRoundRobin extends PreemptiveScheduler {
 		time = 0;
 		Integer quantum = super.quantum;
 
+		Deque<String> ready = new ArrayDeque<>();
+		processes.stream()
+			.filter(p -> p.getArriveTime() == 0)
+			.forEach(p -> ready.add(p.getId()));
 
+		while (ProcessManager.isNotAllFineshed(processes)) {
+			if (ready.isEmpty()) {
+				log.info(">>> [" + time + "] idle");
+				journal.cpuReport("idle");
+				
+				time++;
+				processes.stream().filter(p -> p.getArriveTime() == time).forEach(p -> {
+					ready.add(p.getId());
+				});
+			} else {
+				String nextProcessID = ready.poll();
+				OSProcess process = processes.stream()
+						.filter(p -> p.getId() == nextProcessID)
+						.collect(Collectors.toList())
+						.get(0);
+
+				Integer timeSlice = 0;
+				while (process.isNotFineshed() && timeSlice < quantum) {
+					log.info(">>> [" + time + "] runing " + process.getId());
+					process.run(time);
+					journal.cpuReport(process.getId());
+					timeSlice++;
+					log.info(">> timeSlice = " + String.valueOf(timeSlice));
+
+					time++;
+					processes.stream()
+						.filter(p -> p.getArriveTime() == time)
+						.forEach(p -> {
+							ready.add(p.getId());
+						});
+
+					if (timeSlice == quantum && process.isNotFineshed()) {
+						log.info(">> process is coming back to ready queue");
+						ready.addLast(process.getId());
+					}
+
+				}
+			}
+		}
 	}
 
 }
